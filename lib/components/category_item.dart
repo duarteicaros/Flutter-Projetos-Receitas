9879e63a95219993eed897f30af73c124b5e4b31
Ícaro Carvalho <icaroduartec@gmail.com>
2022-09-23 21:00:10 -0300

    Criação de navegação + Nova base de dados de receitas

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,30 +1,49 @@
 import 'package:flutter/material.dart';
 
 import '../models/category.dart';
+import '../utils/app_routes.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   const CategoryItem(this.category, {super.key});
 
+  void _selectCategory(BuildContext context) {
+    // Navigator.of(context).push(
+    //   MaterialPageRoute(builder: (_) {
+    //     return CategoriesMealsScreen(category);
+    //   }),
+    // );
+
+    Navigator.of(context).pushNamed(
+      AppRoutes.CATEGORIES_MEALS,
+      arguments: category,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      borderRadius: BorderRadius.circular(15),
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleMedium,
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import './screens/categories_screen.dart';
+import './screens/categories_meals_screen.dart';
+import './utils/app_routes.dart';
 
 void main() => runApp(const MyApp());
 
@@ -31,7 +33,11 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: CategoriesScreen(),
+      // initialRoute: '/',
+      routes: {
+        AppRoutes.HOME: (context) => const CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (context) => const CategoriesMealsScreen(),
+      },
     );
   }
 }
